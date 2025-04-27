95bb06dc9acffbda9bea39825d775a6f73987c32
Leo Mclaughlin <leo.mcl616@gmail.com>
2025-04-27 16:27:36 +0100

    a

--- scriptName.sh
+++ scriptName.sh
@@ -1,13 +1,5 @@
-#!/bin/bash
-#
-#SBATCH --job-name=helloMPI        # Job name
-#SBATCH --ntasks=1                 # Default number of MPI ranks
-#SBATCH --time=01:00:00            # Walltime (HH:MM:SS)
-#SBATCH --partition=standard       # Partition/queue
-#SBATCH --output=helloMPI_%j.out   # STDOUT: helloMPI_<jobID>.out
-#SBATCH --error=helloMPI_%j.err    # STDERR: helloMPI_<jobID>.err
 
-# Load Intel compilers and MPI
+# Lo d Intel compilers and MPI
 module load compilers/intel/2019u5
 module load mpi/intel-mpi/2019u5/bin
 
@@ -15,7 +7,7 @@ module load mpi/intel-mpi/2019u5/bin
 numMPI=${SLURM NTASKS:-1}
 
 # Compile the MPI programme
-mpiicc helloMPI.c
+mpiicc program.c
 
 # Launch exactly that many ranks
-mpirun -np $numMPI ./helloMPI.exe
+mpirun -np $numMPI ./program.exe
