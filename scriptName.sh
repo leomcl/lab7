ca4c6d18ed82ce9e673f25587228eda14d0d4e52
Leo Mclaughlin <leo.mcl616@gmail.com>
2025-04-27 16:26:06 +0100

    a

--- scriptName.sh
+++ scriptName.sh
@@ -12,10 +12,10 @@ module load compilers/intel/2019u5
 module load mpi/intel-mpi/2019u5/bin
 
 # At run‐time SLURM_NTASKS will be set to what you passed via `-n`
-numMPI=${SLURM_NTASKS}
+numMPI=${SLURM NTASKS:-1}
 
 # Compile the MPI programme
-mpiicc helloMPI.c -o helloMPI.exe
+mpiicc helloMPI.c
 
 # Launch exactly that many ranks
-mpirun -np ${numMPI} ./helloMPI.exe
+mpirun -np $numMPI ./helloMPI.exe
