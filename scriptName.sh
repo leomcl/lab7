0c0f0d22071392cccfe9d235aac716a6761e9ae3
Leo Mclaughlin <leo.mcl616@gmail.com>
2025-04-27 16:23:42 +0100

    up

--- scriptName.sh
+++ scriptName.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #
-#SBATCH --job-name=helloMPI       # Job name
-#SBATCH --ntasks=${SLURM_NTASKS:-1}  # Number of MPI ranks (overridable by sbatch -n)
-#SBATCH --time=01:00:00           # Walltime (HH:MM:SS)
-#SBATCH --partition=standard      # Queue/partition name
-#SBATCH --output=helloMPI_%j.out  # STDOUT output file (%j expands to jobID)
-#SBATCH --error=helloMPI_%j.err   # STDERR output file
+#SBATCH --job-name=helloMPI        # Job name
+#SBATCH --ntasks=1                 # Default number of MPI ranks
+#SBATCH --time=01:00:00            # Walltime (HH:MM:SS)
+#SBATCH --partition=standard       # Partition/queue
+#SBATCH --output=helloMPI_%j.out   # STDOUT: helloMPI_<jobID>.out
+#SBATCH --error=helloMPI_%j.err    # STDERR: helloMPI_<jobID>.err
 
 # Load Intel compilers and MPI
 module load compilers/intel/2019u5
 module load mpi/intel-mpi/2019u5/bin
 
-# Determine number of MPI tasks
-numMPI=${SLURM_NTASKS:-1}
+# At run‐time SLURM_NTASKS will be set to what you passed via `-n`
+numMPI=${SLURM_NTASKS}
 
 # Compile the MPI programme
 mpiicc helloMPI.c -o helloMPI.exe
 
-# Run with the requested number of processes
+# Launch exactly that many ranks
 mpirun -np ${numMPI} ./helloMPI.exe
